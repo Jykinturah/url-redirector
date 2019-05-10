45fc33864be5a13409962864c367c03b82b516d4
jykinturah <jykinturah@gmail.com>
2019-05-10 13:53:05 -0700

    Downsize shorten-url app, move to other site.

--- server.js
+++ server.js
@@ -2,12 +2,8 @@
 
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
 const path = require('path');
-const dns = require('dns');
-const nanoid = require('nanoid');
-const validator = require('validator');
+const mongoose = require('mongoose');
 
 const dbUri = encodeURIComponent(process.env.DBURI);
 const dbUser = encodeURIComponent(process.env.DBUSER);
@@ -18,46 +14,11 @@ const dbName = process.env.DB;
 
 /** Mongoose Start **/
 
-const mongoose = require('mongoose');
-
 mongoose.connect(`mongodb://${dbUser}:${dbPswd}@${dbUri}/${dbName}`,{useNewUrlParser: true})
   .then(() => {
     console.log('Database connection successful!');
   })
   .catch(err => {console.error('Database connection error!')});
-
-let shortUrlSchema = new mongoose.Schema({
-  original_url: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: (value) => {return validator.isURL(value)}
-  },
-  short_id: {
-    type: String,
-    required: true,
-    unique: true
-    validate: (value) => {return validator.isAlphanumeric(value)}
-  }
-});
-
-let userSchema = new mongoose.Schema({
-  username: String,
-  hash: String,
-  salt: String
-});
-
-userSchema.methods.setPassword = function(password) {
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-};
-
-userSchema.methods.validatePassword = function(password) {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-  return this.hash === hash;
-};
-
-module.exports = mongoose.model('shortUrl', shortUrlSchema);
 
 /*** Mongoose End ***/
 
@@ -95,11 +56,12 @@ function shortUrl(db, url, id){
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
-app.use(bodyParser.json());
-app.use(session({
-  
-}));
-app.post('/new',(req,res)=>{console.log(req.body);});
+
+app.get('/:short_id', (req, res) => {
+  const shortId = req.params.short_id;
+});
+
+
 app.set('port',process.env.PORT || 4100);
 
 const server = app.listen(app.get('port'),()=>{console.log(`Express running → PORT ${server.address().port}`);});
