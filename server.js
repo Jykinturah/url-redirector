ef9fa343f7246a49b9e9ebc24e4e93dce8d06c4a
jykinturah <jykinturah@gmail.com>
2019-05-10 14:51:05 -0700

    URL redirect working, no web-interface for adding new urls yet

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const dbUri = encodeURIComponent(process.env.DBURI);
 const dbUser = encodeURIComponent(process.env.DBUSER);
 const dbPswd = encodeURIComponent(process.env.DBPWD);
-const dbAuth = process.env.DBAUTH;
 const dbName = process.env.DB;
-// const dbUrl = `mongodb://${dbUser}:${dbPswd}@${dbUri}/?authMechanism=${dbAuth}&authSource=${dbName}`;
 
 /** Mongoose Start **/
 
@@ -20,49 +19,34 @@ mongoose.connect(`mongodb://${dbUser}:${dbPswd}@${dbUri}/${dbName}`,{useNewUrlPa
   })
   .catch(err => {console.error('Database connection error!')});
 
+const shortUrl = require('./schemas/shortUrlSchema.js')(mongoose); // Grab the Schema from git submodule
+
 /*** Mongoose End ***/
 
-/** MongoDB Start **/
-
-/*
-
-const { MongoClient } = require('mongodb');
-
-MongoClient.connect(dbUrl,{useNewUrlParser: true}).then(client => {console.log("Connected!");client.close();}).catch((err) => console.error(err));
-
-function shortUrl(db, url){return shortUrl(db,url,null)};
-
-function shortUrl(db, url, id){
-  const shortdb = db.collection('shortenedURLs');
-  if(id){
-    return shortenedURLs.findOneAndUpdate(
-      { short_id: id },
-      { original_url: url, short_id: id },
-      { returnOriginal: false, upsert: true }
-    );
- }else{
-    return shortenedURLs.findOneAndUpdate(
-      { original_url: url },
-      { $setOnInsert: { original_url: url, short_id: nanoid(8) } },
-      { returnOriginal: false, upsert: true }
-    );
-  }
-}
-*/
-
-/*** MongoDB End ***/
-
 /** Express Start **/
+
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/:short_id', (req, res) => {
-  const shortId = req.params.short_id;
+  var shortId = req.params.short_id;
+  console.log(shortId);
+  if (!validator.isAlphanumeric(shortId)) { 
+    console.log("Not Alphanumeric"); 
+    res.redirect("/");
+  } else {
+    shortUrl.findOne({ short_id: `${shortId}` }, 
+      (err, shortDoc) => {
+        if( shortDoc === null ) { console.log("Not in DB"); res.redirect("/"); } 
+        else res.redirect(shortDoc.original_url); 
+      }
+    );
+  }
 });
-
 
 app.set('port',process.env.PORT || 4100);
 
 const server = app.listen(app.get('port'),()=>{console.log(`Express running → PORT ${server.address().port}`);});
+
 /*** Express End ***/
